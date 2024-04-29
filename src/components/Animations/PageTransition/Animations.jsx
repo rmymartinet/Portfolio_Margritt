547d97cb6cc76c13ba57adddb15a4b23eb264478
Martinet <remymartinet.contact@gmail.com>
2024-04-29 10:51:51 +0200

    updated transition between pages

--- src/components/Animations/PageTransition/Animations.jsx
+++ src/components/Animations/PageTransition/Animations.jsx
@@ -1,4 +1,4 @@
-import { Power1 } from "gsap";
+import { Power1, Power2 } from "gsap";
 
 export const childrenAnimation = {
   initial: {
@@ -18,6 +18,55 @@ export const childrenAnimation = {
     transition: {
       ease: Power1.easeIn,
       duration: 1,
+    },
+  },
+};
+
+export const slideOutAnimation = {
+  initial: {
+    scaleY: 0,
+  },
+  animate: {
+    scaleY: 0,
+  },
+  exit: {
+    scaleY: 1,
+    transition: {
+      duration: 1,
+      ease: Power1.easeOut,
+    },
+  },
+};
+
+export const slideInAnimation = {
+  initial: {
+    scaleY: 1,
+  },
+  animate: {
+    scaleY: 0,
+    transition: {
+      duration: 1,
+      ease: Power2.easeOut,
+    },
+  },
+  exit: {
+    scaleY: 0,
+  },
+};
+
+export const animateSlideImg = {
+  initial: {
+    y: 0,
+  },
+  animate: {
+    y: 0,
+  },
+  exit: {
+    y: -1000,
+    transition: {
+      delay: 0, // Augmenter le délai avant le début de l'animation
+      duration: 2,
+      ease: Power1.easeOut,
     },
   },
 };
--- src/components/Animations/PageTransition/Transition.jsx
+++ src/components/Animations/PageTransition/Transition.jsx
@@ -1,16 +1,37 @@
 import { motion } from "framer-motion";
-import { childrenAnimation } from "./Animations";
+import {
+  childrenAnimation,
+  slideInAnimation,
+  slideOutAnimation,
+} from "./Animations";
+import "./Transition.scss";
 
 export default function Transition({ children, isClicked }) {
   return !isClicked ? (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      variants={childrenAnimation}
-    >
-      {children}
-    </motion.div>
+    <>
+      <motion.div
+        variants={slideInAnimation}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="slide-in"
+      ></motion.div>
+      <motion.div
+        variants={slideOutAnimation}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="slide-out"
+      ></motion.div>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={childrenAnimation}
+      >
+        {children}
+      </motion.div>
+    </>
   ) : (
     <>{children}</>
   );
